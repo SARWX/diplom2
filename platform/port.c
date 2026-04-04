027b8fddbdf5c6b93494ae05fad187186f8c4edd
Timofei <tictacnumt@gmail.com>
2026-04-05 00:19:37 +0300

    uart logic was moved so delete dead code

--- platform/port.c
+++ platform/port.c
@@ -27,8 +27,6 @@
 #define lcd_init(x)					LCD_Configuration(x)
 #define touch_screen_init(x)		No_Configuration(x)
 
-//#define USART_SUPPORT
-
 /* DW1000 IRQ handler definition. */
 port_deca_isr_t port_deca_isr = NULL;
 
@@ -290,42 +288,6 @@ int RCC_Configuration(void)
         RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO,
         ENABLE);
 
-    return 0;
-}
-
-int USART_Configuration(void)
-{
-#if 0
-	USART_InitTypeDef USART_InitStructure;
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	// USARTx setup
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USARTx, &USART_InitStructure);
-
-	// USARTx TX pin setup
-	GPIO_InitStructure.GPIO_Pin = USARTx_TX;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_Init(USARTx_GPIO, &GPIO_InitStructure);
-
-	// USARTx RX pin setup
-	GPIO_InitStructure.GPIO_Pin = USARTx_RX;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_Init(USARTx_GPIO, &GPIO_InitStructure);
-
-	// Enable USARTx
-	USART_Cmd(USARTx, ENABLE);
-#endif
     return 0;
 }
 
